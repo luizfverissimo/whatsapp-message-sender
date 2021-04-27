9ccbead8314b7936a43f6c4c7f8a45a7dd244817
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-04-27 14:27:45 -0300

    react and electron integration DONE

---
+++ main.js
@@ -0,0 +1,23 @@
+const { BrowserWindow, app } = require('electron');
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    backgroundColor: '#fff',
+    webPreferences: {
+      nodeIntegration: true,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+})
+
+app.whenReady().then(createWindow);
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Olá mundo!!!!!</h1>
+      <p>teste</p>
+    </div>
+  )
+}
+
+export default App
